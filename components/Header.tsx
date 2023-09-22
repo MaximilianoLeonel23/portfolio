a570321203537373f47345410828d40649c51b37
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-09-22 12:20:09 -0300

    feat: add mail and some styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+'use client';
+import { LogoIcon } from '@/assets/icons/generalIcons';
+import { useLanguageStore } from '@/store/useLanguageStore';
+import { useTheme } from 'next-themes';
+import Link from 'next/link';
+import React, { useState } from 'react';
 
 const Header = () => {
+	const { language } = useLanguageStore();
+	const { theme } = useTheme();
 	return (
-		<header className='container flex justify-between items-center py-4 bg-gray-50 dark:bg-gray-800'>
-			<h4 className='font-bold text-gray-800 dark:text-gray-100 text-xl'>
-				Fullstack <br /> Developer
-			</h4>
-			<button className='font-bold bg-primary-teal text-gray-50 dark:text-gray-800 rounded-full px-6 py-2'>
-				Contact
-			</button>
+		<header className='container flex justify-between items-start py-4 bg-gray-50 dark:bg-gray-800'>
+			<div>
+				<LogoIcon mode={theme === 'dark' ? 'dark' : 'light'} />
+			</div>
+			<Link
+				href='mailto:maxoleo.dev@gmail.com'
+				className='font-bold text-xs sm:text-sm bg-primary-teal text-gray-50 w-fit dark:text-gray-800 rounded-full px-6 py-2 transition-all duration-300'
+			>
+				{language === 'en' ? 'I wanna meet you!' : 'Quiero conocerte!'}
+			</Link>
 		</header>
 	);
 };
--- components/cards/ModeCard.tsx
+++ components/cards/ModeCard.tsx
@@ -5,15 +5,15 @@ import { useTheme } from 'next-themes';
 
 const ModeCard = () => {
 	const { theme, setTheme } = useTheme();
+
 	return (
 		<article
 			onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-			className='relative aspect-square flex items-center justify-center p-4 rounded-2xl bg-gray-100  dark:bg-gray-700 cursor-pointer'
+			className={`relative aspect-square flex items-center justify-center p-4 rounded-2xl dark:bg-gray-700 cursor-pointer z-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-yellow-100 to-gray-100 dark:from-transparent from-10% to-60% dark:to-transparent transition-all duration-700`}
 		>
-			<div className='w-3/5 h-3/5 icon-fullwidth'>
+			<div className='w-3/5 h-3/5 icon-fullwidth z-20'>
 				<LampIcon mode={theme === 'dark' ? 'dark' : 'light'} />
 			</div>
-			<div className='absolute top-0 left-0 w-full h-full z-20 mode-background'></div>
 		</article>
 	);
 };
